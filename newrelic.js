6b06a66f5d79e194b2f478fd7433f7434bc7f11e
Martin Obredor <martin.obredor@gmail.com>
2024-12-13 01:43:46 -0300

    new relic config

--- newrelic.js
+++ newrelic.js
@@ -1,11 +1,18 @@
+const { enable } = require(".");
+
 exports.config = {
   app_name: ['devops-project-up'],
   license_key: '36301150aeb5e7c59f33693e477cce6cFFFFNRAL',
   logging: {
     level: 'trace',
+    enabled: true
   },
   application_logging: {
+    enabled: true, 
     forwarding: {
+      enabled: true
+    },
+    metrics:{
       enabled: true
     }
   }  
